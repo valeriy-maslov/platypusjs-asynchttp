b834574b97566a50b393be0621a2c3fcdeae10c8
Valeriy Maslov <vmaslov@altsoft.biz>
2016-07-05 12:51:38 +0300

    First test fix

--- app/src/com/hcspider/platypusjs/asynchttp/AsyncHTTP.js
+++ app/src/com/hcspider/platypusjs/asynchttp/AsyncHTTP.js
@@ -3,6 +3,7 @@
  */
 define(['logger','./HttpAsyncClientsFactory','./HttpRequestFactory','./AsyncProcess'],function(Log,ClientsFactory,RequestFactory,Async) {
     var Future = Java.type('java.util.concurrent.Future');
+    var EntityUtils = Java.type('org.apache.http.util.EntityUtils');
     /**
      * @class
      */
@@ -20,7 +21,11 @@ define(['logger','./HttpAsyncClientsFactory','./HttpRequestFactory','./AsyncProc
         return {
             execute: function(onSuccess,onFailure) {
                 try {
-                    client.execute(request,Async.wrapCallbacks(onSuccess,onFailure));
+                    client.execute(request,Async.wrapCallbacks(function(result) {
+                        var entity = result.getEntity();
+                        var content = EntityUtils.toString(entity);
+                        onSuccess(content);
+                    },onFailure));
                 } catch (e) {
                     Log.severe(e);
                 }
